3b8982b8b253b3ff7d4d5d4bfe7602d20e2b4b52
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-09-29 08:20:12 +0300

    feat: initial project

---
+++ src/main/java/exam/prog2Rp/Prog2RpApplication.java
@@ -0,0 +1,13 @@
+package exam.prog2Rp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog2RpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog2RpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exam/prog2Rp/Prog2RpApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.prog2Rp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog2RpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
